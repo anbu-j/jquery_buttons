517929f75f2a433e35fca3092269f36e288d7787
PSADMIN <psadmin@psadmins-air-8.attlocal.net>
2021-05-16 19:53:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#q1").click(function(){
+        $("#answer").text("Anbu")
+    
+
+    })
+$("#q2").click(function(){
+        $("#answer").text("Atlanta")
+    
+
+    })
+    $("#q3").click(function(){
+        $("#answer").text("Btech")
+    
+
+    })
+    $("#q4").click(function(){
+        $("#answer").text("31")
+    
+
+    })
+});
